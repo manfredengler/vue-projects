7df7b3b2e4e1b3e96f40d4de19da5a6b3f89d68d
manfred <manfred.engler.95@gmail.com>
2024-03-11 21:55:09 -0300

    Agregado componente InputTag a App.vue

--- tags/src/App.vue
+++ tags/src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
+import InputTag from './components/InputTag.vue'
 </script>
 
-<template></template>
+<template>
+  <InputTag />
+</template>
 
 <style scoped></style>
---
+++ tags/src/components/InputTag.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <div class="inputTag">
+        <div class="tags">
+            <input type="text" />
+
+        </div>
+    </div>
+</template>
+
+
+<style></style>
